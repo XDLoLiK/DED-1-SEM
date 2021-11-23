157f5c90896db68c3fd862eccb2c488c80905ed4
XDLoLiK <66733765+XDLoLiK@users.noreply.github.com>
2021-11-24 00:09:30 +0300

    Add files via upload

--- Differentiator/main.cpp
+++ Differentiator/main.cpp
@@ -1,1 +1,32 @@
+/**
+ * @defgroup   MAIN main
+ *
+ * @brief      This file implements main.
+ *
+ * @author     Stas
+ * @date       2021
+ */
 
+
+#include "Differentiator/Differentiator.h"
+
+
+int main()
+{
+	Logger_Init();
+
+	bool running = true;
+
+	while (running)
+	{
+		char expression[MAX_EXPRESSION_LEN] = "";
+		Differentiator_ScanExpression(expression);
+
+		Tree_t* expressionTree = Differentiator_CreateTree(expression);	
+		Differentiator_Differentiate (&expressionTree);
+	}
+
+	Logger_Finish();
+
+	return 0;
+}
