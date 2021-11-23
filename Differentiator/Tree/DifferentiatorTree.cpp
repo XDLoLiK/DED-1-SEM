0cc94eea12e480857ac12f1599a8f9c2387524d3
XDLoLiK <66733765+XDLoLiK@users.noreply.github.com>
2021-11-24 00:08:57 +0300

    Add files via upload

---
+++ Differentiator/Tree/DifferentiatorTree.cpp
@@ -0,0 +1,14 @@
+/**
+ * @defgroup   DIFFERENTIATORTREE Differentiator Tree
+ *
+ * @brief      This file implements Differentiator Tree.
+ *
+ * @author     Stas
+ * @date       2021
+ */
+
+
+#include "DifferentiatorTree.h"
+
+
+
--- Differentiator/Tree/DifferentiatorTree.h
+++ Differentiator/Tree/DifferentiatorTree.h
@@ -1,1 +1,33 @@
+/**
+ * @defgroup   DIFFERENTIATORTREE Differentiator Tree
+ *
+ * @brief      This file implements Differentiator Tree.
+ *
+ * @author     Stas
+ * @date       2021
+ */
 
+
+#ifndef DIFFERENTIATOR_TREE_H
+#define DIFFERENTIATOR_TREE_H
+
+
+typedef struct node_t
+{
+	int type  = 0;
+	int value = 0; 
+
+	node_t* left  = nullptr;
+	node_t* right = nullptr;
+
+} Node_t;
+
+
+typedef struct tree_t
+{
+	node_t* root = nullptr;
+
+} Tree_t;
+
+
+#endif // DIFFERENTIATOR_TREE_H
