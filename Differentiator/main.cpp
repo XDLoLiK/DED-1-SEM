ca9f515f8bbe498f204608fcec2335ef3ce4179b
XDLoLiK <66733765+XDLoLiK@users.noreply.github.com>
2022-06-09 22:45:38 +0300

    Add files via upload

--- Differentiator/main.cpp
+++ Differentiator/main.cpp
@@ -15,18 +15,41 @@ int main()
 {
 	Logger_Init();
 
-	bool running = true;
+	int ord = 1;
 
-	while (running)
-	{
-		char expression[MAX_EXPRESSION_LEN] = "";
-		Differentiator_ScanExpression(expression);
+	printf("Order: ");
 
-		Tree_t* expressionTree = Differentiator_CreateTree(expression);	
-		Differentiator_Differentiate (&expressionTree);
-	}
+	scanf(" %d ", &ord);
+
+	char expr[100] = "";
+
+	gets(expr);
+
+	printf("\n\n");
+
+
+	Tree_t* expressionTree = Differentiator_CreateTree(expr);
+
+	expressionTree->root   = Differentiator_HigherOrderDerivative(expressionTree->root, ord);
+
+	// expressionTree->root = Differentiator_TaylorExpansion(expressionTree->root, ord, 0);
+
+	expressionTree->root = Differentiator_Simplify(expressionTree->root);
+
+	DifferentiatorTree_DumpTree(expressionTree->root);
+
+	printf("Finally, we get:\n");
+
+	Differentiator_ShowResult(stdout, expressionTree->root);
+
+	printf("\n\n");
+
+	FILE* dumpFile = fopen("derivative.txt", "w");
+
+	Differentiator_DumpToTex(dumpFile, expressionTree->root);
 
 	Logger_Finish();
 
 	return 0;
 }
+	
